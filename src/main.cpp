853574df783a6d55abc622b4fbd71553eed80c03
Adrian Aquino-Neary <adrianaquino.aquino33@gmail.com>
2026-05-16 23:01:54 +0100

    Drag & drop test

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,17 @@
 #include "image/Image.h"
 #include "wrapper/Log.h"
 #include <cstdlib>
+#include <string>
 
 int main(int argc, char* argv[]) {
-	Log::WriteOneLine("Hello World!");
-	
-	Image img(256, 256, 3);
-	img.Write("data/test.png");
-	Log::Save("data/console.log");
+	Log::WriteOneLine("argc: " + Log::ToString(argc));
+
+	for (int i = 0; i < argc; ++i) {
+		Log::WriteOneLine("  " + static_cast<std::string>(argv[i]));
+	}
+
+	//Log::HoldConsole();
+	Log::Save("console.log");
 
 	return EXIT_SUCCESS;
 }
